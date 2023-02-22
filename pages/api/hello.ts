7e557b4e2a86883f2e72bda84a7f9e4f3d30768b
Pau <raigroig@hotmail.es>
2023-02-22 19:56:32 +0100

    wip

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next"
-import prisma from "../../lib/prisma"
+//import prisma from "../../lib/prisma"
 
 import vehicle from "../../data/vehicle.json"
 
@@ -12,6 +12,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<typeof vehicle>
 ) {
-  const users = await prisma.user.findMany()
+  //const users = await prisma.user.findMany()
   res.status(200).json(vehicle)
 }
