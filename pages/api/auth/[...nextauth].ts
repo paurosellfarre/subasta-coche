b068a4f66eef3914932a645c66a7606421132134
Pau <raigroig@hotmail.es>
2023-02-27 18:53:55 +0100

    wip

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,12 +2,14 @@ import NextAuth from "next-auth"
 import type { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import prisma from "../../../utils/prisma"
+import prisma from "@utils/prisma"
+import bcrypt from "bcryptjs"
 
 export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
+
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
@@ -18,16 +20,32 @@ export const authOptions: NextAuthOptions = {
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
       },
       async authorize(credentials, req) {
+        const email = credentials?.email
+        const password = credentials?.password
+
+        if (!email || !password) return null
+
         // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials?.email,
+          },
+        })
 
         if (user) {
+          //Compare password
+          const isPasswordCorrect = await bcrypt.compare(
+            password,
+            user.password
+          )
+
+          if (!isPasswordCorrect) return null
           // Any object returned will be saved in `user` property of the JWT
-          return user
+          return { email: user.email, name: user.name, id: user.id } as any
         } else {
           // If you return null then an error will be displayed advising the user to check their details.
           return null
