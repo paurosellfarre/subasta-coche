7f30d0039b9c0e5710c2bd03830fddde839f0486
Pau <raigroig@hotmail.es>
2023-03-01 13:05:28 +0100

    Add delete uploaded image button

--- app/(site)/publicar-anuncio/page.tsx
+++ app/(site)/publicar-anuncio/page.tsx
@@ -124,7 +124,7 @@ export default function PublicarAnuncio() {
         disabled={isFetching}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
-        Submit
+        Publicar
       </button>
     </form>
   )
--- components/Upload/UploadImages.tsx
+++ components/Upload/UploadImages.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import base64 from "@utils/base64"
+import Image from "next/image"
 
 export default function UploadImages({ setFormData, formData }: any) {
   const uploadImageToClient = async (event: any) => {
@@ -27,42 +28,69 @@ export default function UploadImages({ setFormData, formData }: any) {
   return (
     <div>
       <label className="block text-sm font-medium text-gray-700">
-        Cover photo
+        Imágenes del vehiculo
       </label>
-      <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-        <div className="space-y-1 text-center">
-          <svg
-            className="mx-auto h-12 w-12 text-gray-400"
-            stroke="currentColor"
-            fill="none"
-            viewBox="0 0 48 48"
-            aria-hidden="true"
-          >
-            <path
-              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-          <div className="flex text-sm text-gray-600">
-            <label
-              htmlFor="file-upload"
-              className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
+      <div className="mt-1 mb-6 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
+        <div className="space-y-1 text-center justify-center">
+          {formData.images.create.length === 0 ? (
+            <svg
+              className="mx-auto h-12 w-12 text-gray-400"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 48 48"
+              aria-hidden="true"
             >
-              <span>Upload a file</span>
+              <path
+                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          ) : (
+            //Show the images preview
+            <div className="grid grid-cols-2 lg:grid-cols-5 mb-5 justify-around">
+              {formData.images.create.map((image: any) => (
+                <div>
+                  <button
+                    className="absolute bg-red-500 text-white rounded-full w-5 h-5 flex justify-center items-center"
+                    onClick={() => {
+                      setFormData((prevFormData: typeof formData) => ({
+                        ...prevFormData,
+                        //Remove the image from the images array
+                        images: {
+                          create: prevFormData.images.create.filter(
+                            (img: any) => img.name !== image.name
+                          ),
+                        },
+                      }))
+                    }}
+                  >
+                    X
+                  </button>
+                  <Image
+                    key={image.name}
+                    src={image.binaryFile}
+                    alt={image.name}
+                    width={100}
+                    height={70}
+                    className="p-1"
+                  />
+                </div>
+              ))}
+            </div>
+          )}
+          <div className="flex justify-center text-sm text-gray-600">
+            <label className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500">
+              <span>Seleccionar Imágen</span>
               <input
-                id="file-upload"
-                name="file-upload"
                 type="file"
                 className="sr-only"
                 onChange={uploadImageToClient}
               />
             </label>
-
-            <p className="pl-1">or drag and drop</p>
           </div>
-          <p className="text-xs text-gray-500">PNG or JPG up to 10MB</p>
+          <p className="text-xs text-gray-500">PNG o JPG hasta 10MB</p>
         </div>
       </div>
     </div>
