92d67fd79397082e6d8c3f27a835ce88fc8fc9fc
Pau <raigroig@hotmail.es>
2023-04-10 20:15:15 +0200

    Add Home Page paginated grid ads component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,40 +1,18 @@
-import AdCard from "@components/Card/AdCard"
-import { Prisma } from "@prisma/client"
+import AutomobileAdsGrid from "@components/Gallery/AutomobileAdsGrid"
 
-async function fetchAllAutomobiles() {
-  const automobiles = await fetch(process.env.PUBLIC_URL + "/api/automobile", {
-    cache: "no-store",
-  })
-  return automobiles.json()
+interface PageProps {
+  searchParams?: {
+    page?: string
+  }
 }
 
-export default async function Home() {
-  const automobiles = await fetchAllAutomobiles()
+export default async function Home(props: PageProps) {
+  const page = props.searchParams?.page
+  const pageNum = typeof page === "string" ? parseInt(page) : 1
 
   return (
-    <section>
-      <div className="bg-white">
-        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900 text-center">
-            Subastas de vehiculos a punto de terminar
-          </h2>
-
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-3 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-            {automobiles.map(
-              (
-                automobile: Prisma.AutomobileGetPayload<{
-                  include: { images: true }
-                }>
-              ) => (
-                <AdCard
-                  key={automobile.id}
-                  automobile={automobile}
-                />
-              )
-            )}
-          </div>
-        </div>
-      </div>
-    </section>
+    <main>
+      <AutomobileAdsGrid page={pageNum || 1} />
+    </main>
   )
 }
