b7960c99f0fe7dee84f062d6570ca7beb4ca0f17
Pau <raigroig@hotmail.es>
2023-04-10 22:14:16 +0200

    Try PaginationButtons as servercomponents

--- components/Button/PaginationButtons.tsx
+++ components/Button/PaginationButtons.tsx
@@ -1,38 +1,43 @@
-"use client"
-
 import Link from "next/link"
 
 interface PaginationButtonsProps {
   currentPage: number
   disableNextPage?: boolean
   route?: string
-  handlePrev?: () => void
-  handleNext?: () => void
 }
-const PaginationButtons = ({ currentPage, route }: PaginationButtonsProps) => {
+
+const PaginationButtons = ({
+  currentPage,
+  route,
+  disableNextPage,
+}: PaginationButtonsProps) => {
   return (
     <div className="flex flex-row items-center justify-center">
-      <Link
-        key="Atras"
-        href={`${route}?page=${currentPage - 1}`}
-        className={
-          "bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
-        }
-      >
-        Atras
-      </Link>
+      {currentPage > 1 ? (
+        <Link
+          key="Atras"
+          href={`${route}?page=${currentPage - 1}`}
+          className={
+            "bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+          }
+        >
+          Atras
+        </Link>
+      ) : null}
 
       <p className="mx-4 text-[14px] font-medium">Página {currentPage}</p>
 
-      <Link
-        key="Siguiente"
-        href={`${route}?page=${currentPage + 1}`}
-        className={
-          "bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
-        }
-      >
-        Siguiente
-      </Link>
+      {!disableNextPage ? (
+        <Link
+          key="Siguiente"
+          href={`${route}?page=${currentPage + 1}`}
+          className={
+            "bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+          }
+        >
+          Siguiente
+        </Link>
+      ) : null}
     </div>
   )
 }
